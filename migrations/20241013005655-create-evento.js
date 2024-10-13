a522ca9d5531ce59d831eb85acad1f00131390c5
luanarmo <luisangel_arroyo@outlook.com>
2024-10-12 18:58:30 -0600

    feat: Generar modelo Evento

---
+++ migrations/20241013005655-create-evento.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Eventos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      fecha: {
+        type: Sequelize.DATE
+      },
+      ubicacion: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Eventos');
+  }
+};
